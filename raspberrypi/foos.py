ec6f6a6b295f4ffaa8ef94fb6f1dc06fd700bc3a
Leopold Joy <yellowcelloleo@yahoo.com>
2015-05-12 00:59:26 -0400

    Updated the hardware files (Raspberry Pi and Arduino).

--- raspberrypi/foos.py
+++ raspberrypi/foos.py
@@ -21,48 +21,29 @@ serialport = 3
 ##and you have to restart comp.
 
 
-##message is just so we dont have unidentified var
 
 goals = []
-##safety == False if you want computer to turn off
-##safety == True if you want computer to stay on
-safety = False
 
-##Loop indefinetely
+
 temp1 = '0'
 temp2 = '0'
 i = 0
-p1 = 0
-p2 = 0
 k = 0
-RIN1 = '661374900'
-RIN2 = '665554444'
+
+tableid = '1'
 threshold1 = '0'
 threshold2 = '0'
 
-query_args = {'rin1':RIN1,'rin2':RIN2}
-url1 = 'http://foosrpi.com/start/'
-data = urllib.urlencode(query_args)
-request = urllib2.Request(url1,data)
-GAMEID = urllib2.urlopen(request).read()
 
+query_args = {'table':tableid}
+
+
+##Starting a connection to the serial input.
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+##Loop indefinetely
 while True:
-    if p1 == 10 or p2 == 10:
-        p1 = 0
-        p2 = 0
-        temp1 = '0'
-        temp2 = '0'
-        ##urllib.urlopen('http://foosrpi.edu/end?game='+GAMEID)       
-        k+=1
-	#urllib.urlopen('http://foosrpi.com/end
-	query_args = {'rin1':RIN1,'rin2':RIN2}
-	url1 = 'http://foosrpi.com/start/'
-	data = urllib.urlencode(query_args)
-	request = urllib2.Request(url1,data)
-	GAMEID = urllib2.urlopen(request).read()
-    else:
         if (i == 0):
             message = ser.readline()
             ##getting the value found from the serial port of the value detected
@@ -72,53 +53,47 @@ while True:
             ##getting current time from computer
         
             goals = message.split()
-            ##goals = ['0','0']
+            print goals
+		##goals = ['0','0']
             ##making the voltage into a value we can analyse in program
-	    print goals,"Score:",p1,"-",p2,GAMEID
-			
-            # print goals[0],goals[1],GAMEID,goals[2],goals[3],p1,p2
+
+            ##Making sure that the serial input is properly formatted.
+	    ##If it isn't, maintain the goal state as it was in the previous
+	    ##tick.
+	    
+            
 	    try: 
-		    if(len(goals) == 2 and (int(goals[0])<1000 or  int(goals[0]) >0) and( int(goals[1]) <1000 or  int(goals[1]) >0)):
+		    if(len(goals) == 2 and (int(goals[0])<2000 and  int(goals[0]) >0) and( int(goals[1]) <2000 and  int(goals[1]) >0)):
 		    	
 	    		threshold1 = '0'
 	   		threshold2 = '0'
-            		if(int(goals[0]) < 125):
-	    			threshold1 = '1'
+	   		##If 
+            		if(int(goals[0]) > 970):
+						threshold1 = '1'
 	    	   
-            		if(int(goals[1]) < 125):
-	 			threshold2 = '1'
+            		if(int(goals[1]) > 520):
+						threshold2 = '1'
 
             except ValueError:
 		threshold1 = temp1
 		threshold2 = temp2
 	    
- ##Threshold is defined in the arduino file. at 
-            ##Temp random seed
-            ###random.seed();
-            #time.sleep(.1)
-            #random2 = random.randint(0,10)
-            #print random2
-            #if random2 == 10:
-            #    if random.randint(0,1) == 1:
-            #        goals[0] = '1'
-            #        goals[1] = '0'
-            #    else:
-            #        goals[0] = '0'
-            #        goals[1] = '1'
-                    
-            ##print goals[0],goals[1],GAMEID,p1,p2
+
+	   
+        ##This program only calculates if a goal has been scored and on what side 
             if threshold1  == '0' and temp1 == '1':
                 #print message_list[0]
                 print 'goal one scored on'
                 temp1 = '0'
-                i+=400
-                p1+=1
-		query_args = {'player':'1','game':GAMEID}
+                
+		#query_args = {'player':'1','game':GAMEID}
+		query_args = {'tableId':tableid,'player':'1'}
 		url1 = 'http://foosrpi.com/goal/'
 		data = urllib.urlencode(query_args)
 		request = urllib2.Request(url1,data)
 		print urllib2.urlopen(request).read()
-            else:
+            	time.sleep(4)
+	    else:
                 temp1 = '1'
                 
             
@@ -126,20 +101,21 @@ while True:
                 print 'goal two scored on'
                 #print message_list[1]
                 temp2 = '0'
-                i+=400
-                p2+=1
-                query_args = {'player':'2','game':GAMEID}
+		               
+                #query_args = {'player':'2','game':GAMEID}
+                query_args = {'tableId':tableid,'player':'2'}
                 url1 = 'http://foosrpi.com/goal/'
                 data = urllib.urlencode(query_args)
                 request = urllib2.Request(url1,data)
                 print urllib2.urlopen(request).read()
-
+		time.sleep(4)
             else:
                 temp2 = '1'
             temp1 = threshold1
             temp2 = threshold2
         else:
-           # time.sleep(4)
+            
             message = ser.readline()
-            i-=1
+            
+	    print i
 
---
+++ raspberrypi/piip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ipVar=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+curl https://api.pushbullet.com/v2/pushes \
+-u <paste_your_API_key_here>: \
+-d device_iden="<device_ID_of_your_phone>" \
+-d type="note" \
+-d title="Pi IP address" \
+-d body=$ipVar \
+-X POST
